bb98b2450a3a3de1786b8f8e4226b1c685909a86
Xcf <Xiscf@users.noreply.github.com>
2023-05-06 16:30:45 +0200

    Delete Contents/Core directory

--- Contents/Core/settings.lua
+++
@@ -1,8 +0,0 @@
--- File: settings.lua
--- What's for: We use this file for the option inside the UI,
--- and variables.
-
-BlessingSettings = {};
-
--- Make a parent panel
-BlessingSettings.panel = CreateFrame("Frame", "BlessingOptionPanel", UIParent);
--- Contents/Core/ui.lua
+++
@@ -1,4 +0,0 @@
--- File: ui.lua
--- What's for: We use this file for creating the ui of the addon.
-
-local BlessingUI = CreateFrame("Frame");
